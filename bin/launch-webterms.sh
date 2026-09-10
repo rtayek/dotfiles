cbd38e41d02e06936fccc79659a3ea54e2f22e5a
Ray Tayek <rtayek@gmail.com>
2026-09-09 20:56:59 -0700

    Remove duplicate Clipboard webterm

--- bin/launch-webterms.sh
+++ bin/launch-webterms.sh
@@ -30,4 +30,3 @@ restart_webterm 1033 /c/Users/ray/eclipse-workspace/chatmap
 restart_webterm 1034 /c/Users/ray/eclipse-workspace/five-rules
 restart_webterm 1035 /c/Users/ray/eclipse-workspace/system
 restart_webterm 1036 /c/Users/ray/eclipse-workspace/clipboard
-restart_webterm 1037 /c/Users/ray/eclipse-workspace/clipboard
